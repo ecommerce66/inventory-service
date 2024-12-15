ebb36c0a132eba9ab0a781bd3a5b74b50a9e491d
SanektAnnadate <sanketannadatejain@gmail.com>
2024-12-15 20:29:34 +0530

    first commit

---
+++ src/test/java/com/om/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
